b2c1ede0784c96b01db545c9659d6d000a99e0e4
Legion <tsje_mail@126.com>
2022-12-04 19:21:55 +0100

    all done

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="snowflake",
     version="0.1",
     description="draw 10 snow flowers on screen with defined color",
-    packages=["snow_flaker"],
+    packages=["snowflake"],
     python_requires=">3",
     install_requires=["numpy>=1.19"]
 )
