0c219295e89fb1edce82b1dbf58f1dad9507fd96
Legion <tsje_mail@126.com>
2022-12-04 20:07:06 +0100

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(
     name="snowflake",
     version="0.1",
+    author="dsss_ex5",
+    author_email="dsss_ex5@virtual.com",
     description="draw 10 snow flowers on screen with defined color",
-    packages=["snowflake"],
+    packages=find_packages(),
     python_requires=">3",
-    install_requires=["numpy>=1.19"]
+    install_requires=["numpy","turtles"]
 )
